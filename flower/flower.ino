98bbda884c9bccbf9493165e32a355c141bda0cf
Lycorisradiata7 <soldier198799@gmail.com>
2017-06-14 11:56:32 +0800

    Add files via upload

---
+++ flower/flower.ino
@@ -0,0 +1,171 @@
+        int latchPin = 8;
+        
+        
+        int clockPin = 12;
+        
+        
+        int dataPin = 11;
+        
+
+byte colDataMatrix[8] = { B01111111,
+              
+              
+              B10111111,
+              
+              
+              B11011111,
+              
+              
+              B11101111,
+              
+              
+              B11110111,
+              
+              
+              B11111011,
+              
+              
+              B11111101,
+           
+              
+              B11111110
+
+
+};
+
+byte rowDataMatrix[8] = { B00111100,
+B01100110,
+
+
+B11000011,
+
+
+B10011001,
+
+
+B10011001,
+
+
+B11000011,
+
+
+B01100110,
+
+
+B00111100
+
+
+
+};
+
+byte rowDataMatrix2[8] = { B00000000,
+B00011000,
+
+
+B00111100,
+
+
+B01100110,
+
+
+B01100110,
+
+
+B00111100,
+
+
+B00011000,
+
+
+ B00000000
+
+
+
+};
+
+
+
+
+
+// 定義顯示器顯示的時間 (延遲時間), 預設 1ms
+
+
+int delay_time = 1;
+
+
+
+
+
+
+void setup() {
+
+pinMode(latchPin,OUTPUT);
+pinMode(clockPin,OUTPUT);
+pinMode(dataPin,OUTPUT);
+
+}
+
+
+void loop() {
+
+for(int i = 0; i < 50 ; i++){
+
+for(int i = 0; i < 8 ; i++){
+
+//byte colData = colDataMatrix[i];
+
+digitalWrite(latchPin,LOW);
+
+// pickDigit(i);
+
+shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+
+shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix[i]);
+
+
+//shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+
+
+digitalWrite(latchPin,HIGH);
+
+
+delay(delay_time);
+
+}
+
+}
+
+
+for(int i = 0; i < 50 ; i++){
+for(int i = 0; i < 8 ; i++){
+
+//byte colData = colDataMatrix[i];
+
+digitalWrite(latchPin,LOW);
+
+// pickDigit(i);
+
+shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+
+shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix2[i]);
+
+
+//shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+
+
+digitalWrite(latchPin,HIGH);
+
+
+delay(delay_time);
+}
+
+}
+
+
+
+
+
+}
+
+
+
